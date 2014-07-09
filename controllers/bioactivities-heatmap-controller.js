dd89af4d3c0dd69bae60dac0b2e33ca7c2933d39
Adam Gradzki <adam.gradzki@gmail.com>
2014-07-09 19:45:22 +0200

    d3.js wired up to get data from mps-database-server

--- controllers/bioactivities-heatmap-controller.js
+++ controllers/bioactivities-heatmap-controller.js
@@ -34,32 +34,42 @@ MPS.controller(
                 }
             ).success(
                 function (data) {
-                    $scope.data_url =
-                        "/media/heatmap/"
-                        + data["tsv"]
-                        + ".tsv";
-                    
-                    if (data["tsv"] != undefined) {
-                        console.log("Loading d3 heatmap...");
+
+                    if (data["data_csv"] != undefined &&
+                        data["rows"] != undefined &&
+                        data["cols"] != undefined) {
+
+                        $scope.data_url =
+                            "/media/heatmap/" + data["data_csv"];
+
+                        $scope.heatmap_rows = data["rows"];
+                        $scope.heatmap_cols = data["cols"];
                         $scope.load_d3_heatmap();
+
+                    } else {
+                        console.log("------- critical -------");
+                        console.log("data csv  - " + data["data_csv"]);
+                        console.log("data rows - " + data["rows"]);
+                        console.log("data cols - " + data["cols"]);
                     }
                 }
             ).error(
                 function () {
+                    alert("HTTP Error: Could not get bioactivities data.");
                 }
             );
 
 
             $scope.load_d3_heatmap = function () {
-                
-                var margin = { top: 150, right: 10, bottom: 50, left: 100 },
-                    cellSize = 10;
-                var col_number = 60;
-                var row_number = 50;
+
+                var margin = { top: 150, right: 10, bottom: 50, left: 100 };
+                var cellSize = 10;
+                var col_number = $scope.heatmap_cols.length;
+                var row_number = $scope.heatmap_rows.length;
                 var width = cellSize * col_number;
                 var height = cellSize * row_number;
                 var legendElementWidth = cellSize * 2.5;
-                var colorBuckets = 21;
+
                 var colors = [
                     '#005824', '#1A693B', '#347B53', '#4F8D6B', '#699F83',
                     '#83B09B',
@@ -69,69 +79,18 @@ MPS.controller(
                     '#B14F7C',
                     '#A63467', '#9B1A53', '#91003F'
                 ];
-                var hcrow = [
-                    49, 11, 30, 4, 18, 6, 12, 20, 19, 33, 32, 26, 44, 35, 38, 3,
-                    23, 41, 22, 10,
-                    2, 15, 16, 36, 8, 25, 29, 7, 27, 34, 48, 31, 45, 43, 14, 9,
-                    39, 1, 37, 47,
-                    42, 21, 40, 5, 28, 46, 50, 17, 24, 13
-                ]; // change to gene name or probe id
-                var hccol = [
-                    6, 5, 41, 12, 42, 21, 58, 56, 14, 16, 43, 15, 17, 46, 47,
-                    48, 54, 49,
-                    37, 38, 25, 22, 7, 8, 2, 45, 9, 20, 24, 44, 23, 19, 13, 40,
-                    11, 1, 39,
-                    53, 10, 52, 3, 26, 27, 60, 50, 51, 59, 18, 31, 32, 30, 4,
-                    55, 28, 29,
-                    57, 36, 34, 33, 35
-                ]; // change to gene name or probe id
-                var rowLabel = [
-                    '1759080_s_at', '1759302_s_at', '1759502_s_at',
-                    '1759540_s_at',
-                    '1759781_s_at', '1759828_s_at', '1759829_s_at',
-                    '1759906_s_at',
-                    '1760088_s_at', '1760164_s_at', '1760453_s_at',
-                    '1760516_s_at',
-                    '1760594_s_at', '1760894_s_at', '1760951_s_at',
-                    '1761030_s_at',
-                    '1761128_at', '1761145_s_at', '1761160_s_at',
-                    '1761189_s_at',
-                    '1761222_s_at', '1761245_s_at', '1761277_s_at',
-                    '1761434_s_at',
-                    '1761553_s_at', '1761620_s_at', '1761873_s_at',
-                    '1761884_s_at',
-                    '1761944_s_at', '1762105_s_at', '1762118_s_at',
-                    '1762151_s_at',
-                    '1762388_s_at', '1762401_s_at', '1762633_s_at',
-                    '1762701_s_at',
-                    '1762787_s_at', '1762819_s_at', '1762880_s_at',
-                    '1762945_s_at',
-                    '1762983_s_at', '1763132_s_at', '1763138_s_at',
-                    '1763146_s_at',
-                    '1763198_s_at', '1763383_at', '1763410_s_at',
-                    '1763426_s_at',
-                    '1763490_s_at', '1763491_s_at'
-                ]; // change to gene name or probe id
-                var colLabel = [
-                    'con1027', 'con1028', 'con1029', 'con103', 'con1030',
-                    'con1031',
-                    'con1032', 'con1033', 'con1034', 'con1035', 'con1036',
-                    'con1037',
-                    'con1038', 'con1039', 'con1040', 'con1041', 'con108',
-                    'con109',
-                    'con110', 'con111', 'con112', 'con125', 'con126', 'con127',
-                    'con128',
-                    'con129', 'con130', 'con131', 'con132', 'con133', 'con134',
-                    'con135',
-                    'con136', 'con137', 'con138', 'con139', 'con14', 'con15',
-                    'con150',
-                    'con151', 'con152', 'con153', 'con16', 'con17', 'con174',
-                    'con184',
-                    'con185', 'con186', 'con187', 'con188', 'con189', 'con191',
-                    'con192',
-                    'con193', 'con194', 'con199', 'con2', 'con200', 'con201',
-                    'con21'
-                ]; // change to contrast name
+                var colorBuckets = colors.length;
+
+                var hcrow = [];
+                var hccol = [];
+
+                var i;
+                for (i = 0; i < $scope.heatmap_rows.length; i += 1) {
+                    hcrow[i] = i;
+                }
+                for (i = 0; i < $scope.heatmap_rows.length; i += 1) {
+                    hccol[i] = i;
+                }
 
                 console.log(
                         "d3 will see: " + (
@@ -139,7 +98,7 @@ MPS.controller(
                         )
                 );
 
-                d3.tsv(
+                d3.csv(
                     $scope.data_url,
                     function (d) {
                         return {
@@ -171,7 +130,7 @@ MPS.controller(
                         var colSortOrder = false;
                         var rowLabels = svg.append("g")
                                 .selectAll(".rowLabelg")
-                                .data(rowLabel)
+                                .data($scope.heatmap_rows)
                                 .enter()
                                 .append("text")
                                 .text(
@@ -220,7 +179,7 @@ MPS.controller(
 
                         var colLabels = svg.append("g")
                                 .selectAll(".colLabelg")
-                                .data(colLabel)
+                                .data($scope.heatmap_cols)
                                 .enter()
                                 .append("text")
                                 .text(
@@ -344,9 +303,11 @@ MPS.controller(
                                     )
                                         .select("#value")
                                         .text(
-                                            "lables:" + rowLabel[d.row - 1]
+                                            "lables:"
+                                            + $scope.heatmap_rows[d.row - 1]
                                             + ","
-                                            + colLabel[d.col - 1] + "\ndata:"
+                                            + $scope.heatmap_cols[d.col - 1]
+                                            + "\ndata:"
                                             + d.value
                                             + "\nrow-col-idx:" + d.col + ","
                                             + d.row
