ddcb83c6a153029cab82943f269ede5a041a4722
Adam Gradzki <adam.gradzki@gmail.com>
2014-08-29 19:12:41 -0400

    90% complete target type and organism selection for heatmap

--- controllers/bioactivities-heatmap-selection-controller.js
+++ controllers/bioactivities-heatmap-selection-controller.js
@@ -1,16 +1,27 @@
+/*global MPS:true*/
+
 MPS.controller(
-    'bioactivities_heatmap_selection_controller',
-    [
+    'bioactivities_heatmap_selection_controller', [
         '$scope', '$location', 'bioactivities_heatmap_filter',
-        function ($scope, $location, bioactivities_heatmap_filter) {
+        function($scope, $location, bioactivities_heatmap_filter) {
             'use strict';
 
-            // Give control of the factory to the local $scope:
-            // This is especially important when we are manipulating
-            // data within the factory itself
-            $scope.bioactivities_heatmap_filter = bioactivities_heatmap_filter;
+            $scope.targets = bioactivities_heatmap_filter.targets;
+            $scope.bioactivities = bioactivities_heatmap_filter.bioactivities;
+            $scope.compounds = bioactivities_heatmap_filter.compounds;
+            $scope.normalize_bioactivities = bioactivities_heatmap_filter.normalize_bioactivities;
+            $scope.min_feat_count = bioactivities_heatmap_filter.min_feat_count;
+            $scope.target_types = bioactivities_heatmap_filter.target_types;
+            $scope.organisms = bioactivities_heatmap_filter.organisms;
+            $scope.refresh = bioactivities_heatmap_filter.refresh_all;
 
-            $scope.submit = function () {
+            $scope.$on('heatmap_selection_update', function() {
+                $scope.targets = bioactivities_heatmap_filter.targets;
+                $scope.bioactivities = bioactivities_heatmap_filter.bioactivities;
+                $scope.compounds = bioactivities_heatmap_filter.compounds;
+            });
+
+            $scope.submit = function() {
                 $location.path('/bioactivities/heatmap');
             };
         }
--- factories/bioactivities-heatmap-filter.js
+++ factories/bioactivities-heatmap-filter.js
@@ -1,84 +1,105 @@
+/*global MPS:true*/
+
 MPS.factory(
-    'bioactivities_heatmap_filter', function ($http) {
+    'bioactivities_heatmap_filter', function($rootScope, $http) {
         'use strict';
 
-        var target_types = [
-            {name: 'Cell-Line', is_selected: false},
-            {name: 'Organism', is_selected: false},
-            {name: 'Single Protein', is_selected: false},
-            {name: 'Tissue', is_selected: false}
-        ];
-        var organisms = [
-            {name: 'Homo Sapiens', is_selected: false},
-            {name: 'Rattus Norvegicus', is_selected: false},
-            {name: 'Canis Lupus Familiaris', is_selected: false}
-        ];
+        var normalize_bioactivities = false;
+        var messages = '';
 
+        var target_types = [{
+            name: 'Cell-Line',
+            is_selected: false
+        }, {
+            name: 'Organism',
+            is_selected: false
+        }, {
+            name: 'Single Protein',
+            is_selected: false
+        }, {
+            name: 'Tissue',
+            is_selected: false
+        }];
+        var organisms = [{
+            name: 'Homo Sapiens',
+            is_selected: false
+        }, {
+            name: 'Rattus Norvegicus',
+            is_selected: false
+        }, {
+            name: 'Canis Lupus Familiaris',
+            is_selected: false
+        }];
+
+        var compounds = [];
+        var bioactivities = [];
+        var targets = [];
         var min_feat_count = 10;
-        var compounds = get_all_bioactivities_keys('/bioactivities/all_compounds');
-        var bioactivities = get_all_bioactivities_keys('/bioactivities/all_bioactivities');
-        var targets = get_all_bioactivities_keys('/bioactivities/all_targets');
 
-        var refresh_all = function () {
-            compounds = get_all_bioactivities_keys('/bioactivities/all_compounds');
-            bioactivities = get_all_bioactivities_keys('/bioactivities/all_bioactivities');
-            targets = get_all_bioactivities_keys('/bioactivities/all_targets');
+        var process_data = function(data, resource_url) {
+            var result = [];
+            var i;
+            for (i = 0; i < data.length; i += 1) {
+
+                if (data[i][1] >= min_feat_count) {
+                    result.push({
+                        name: data[i][0],
+                        is_selected: false
+                    });
+                }
+            }
+
+            if (resource_url.search('compound') > -1) {
+                compounds = result;
+            }
+
+            if (resource_url.search('bioactivities') > -1) {
+                bioactivities = result;
+            }
+
+            if (resource_url.search('targets') > -1) {
+                targets = result;
+            }
+
         };
 
-        function get_all_bioactivities_keys(resource_url) {
-            var i;
-            var result = [];
-
-            $http(
-                {
-                    method: 'GET',
-                    params: {
-                        'target_types': target_types,
-                        'organisms': organisms
-                    },
-                    url: resource_url
-                }
-            ).success(
-                function (data) {
-                    var max_length = data.length;
-                    for (i = 0; i < max_length; i += 1) {
-
-                        if (data[i][1] >= min_feat_count) {
-                            result.push(
-                                {name: data[i][0], is_selected: false}
-                            );
-                        }
-                    }
+        var get_all_bioactivities_keys = function(resource_url) {
+            $http({
+                method: 'GET',
+                params: {
+                    'target_types': JSON.stringify(target_types),
+                    'organisms': JSON.stringify(organisms)
+                },
+                url: resource_url
+            }).success(
+                function(data) {
+                    process_data(data, resource_url);
                 }
             ).error(
-                function () {
-                    console.log("get_all_bioactivities_keys request failed.")
+                function() {
+                    console.log("ERROR: get_all_bioactivities_keys FAILED");
                 }
             );
-            return result;
-        }
-
-        return {
-            // expose the private `targets` variable
-            // as a public variable of the same name
-            targets: targets,
-
-            // expose the private `compounds` variable
-            // as a public variable of the same name
-            compounds: compounds,
-
-            // expose the private `bioactivities` variable
-            // as a public variable of the same name
-            bioactivities: bioactivities,
-
-            target_types: target_types,
-
-            organisms: organisms,
-
-            min_feat_count: min_feat_count,
-
-            refresh_all: refresh_all
         };
 
-    }
-);
+        var refresh_all = function() {
+            get_all_bioactivities_keys('/bioactivities/all_targets');
+            get_all_bioactivities_keys('/bioactivities/all_compounds');
+            get_all_bioactivities_keys('/bioactivities/all_bioactivities');
+            $rootScope.$broadcast('heatmap_selection_update');
+        };
+
+        return {
+
+            targets: targets,
+            compounds: compounds,
+            bioactivities: bioactivities,
+            target_types: target_types,
+            organisms: organisms,
+            normalize_bioactivities: normalize_bioactivities,
+            min_feat_count: min_feat_count,
+            refresh_all: refresh_all,
+            messages: messages
+        };
+
+    });
